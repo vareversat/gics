2ae4f85141b54be390389704a55fb3919d6033ef
Valentin REVERSAT <dev@vareversat.fr>
2024-12-21 16:03:19 +0000

    chore(tests): add 2 test files for lexer_test.go

--- parser/lexer_test.go
+++ parser/lexer_test.go
@@ -7,8 +7,34 @@ import (
 	"testing"
 )
 
-func TestEventCalendarComponentParsing(t *testing.T) {
-	input, err := os.OpenFile("./test_data/event.ics", os.O_CREATE, os.ModeAppend)
+func TestEventCalendarComponentParsing_1(t *testing.T) {
+	input, err := os.OpenFile("./test_data/event_1.ics", os.O_CREATE, os.ModeAppend)
+	if err != nil {
+		fmt.Println("Error while opening file:", err)
+		return
+	}
+	defer input.Close()
+	unfoldedData := bytes.Buffer{}
+
+	unfold(input, &unfoldedData)
+	parse(unfoldedData)
+}
+
+func TestEventCalendarComponentParsing_2(t *testing.T) {
+	input, err := os.OpenFile("./test_data/event_2.ics", os.O_CREATE, os.ModeAppend)
+	if err != nil {
+		fmt.Println("Error while opening file:", err)
+		return
+	}
+	defer input.Close()
+	unfoldedData := bytes.Buffer{}
+
+	unfold(input, &unfoldedData)
+	parse(unfoldedData)
+}
+
+func TestEventCalendarComponentParsing_3(t *testing.T) {
+	input, err := os.OpenFile("./test_data/event_3.ics", os.O_CREATE, os.ModeAppend)
 	if err != nil {
 		fmt.Println("Error while opening file:", err)
 		return
