e3598005a374aecf4f0ab18737dfe6a23b09fa7f
Valentin REVERSAT <dev@vareversat.fr>
2024-12-21 15:51:22 +0000

    feat(param-parser): user num instead of hard coded string for switch/case

--- parser/parameters_parser.go
+++ parser/parameters_parser.go
@@ -2,50 +2,51 @@ package parser
 
 import (
 	"github.com/vareversat/gics/parameters"
+	"github.com/vareversat/gics/registries"
 )
 
 // ParseParameter will try to parse a property based on parameterName with the associated parameterValue
 func ParseParameter(parameterName string, parameterValue string) (parameters.Parameter, error) {
 	switch parameterName {
-	case "ALTREP":
+	case string(registries.AlternateTextRepresentationParam):
 		return parameters.NewAlternateTextRepresentationParamFromString(parameterValue)
-	case "CN":
+	case string(registries.CommonNameParam):
 		return parameters.NewCommonNameParam(parameterValue), nil
-	case "CUTYPE":
+	case string(registries.CalendarUserTypeParam):
 		return parameters.NewCalendarUserTypeParamFromString(parameterValue), nil
-	case "DELEGATED-FROM":
+	case string(registries.DelegatedFromParam):
 		return parameters.NewDelegatedFromParamFromString(parameterValue)
-	case "DELEGATED-TO":
+	case string(registries.DelegatedToParam):
 		return parameters.NewDelegatedToParamFromString(parameterValue)
-	case "DIR":
+	case string(registries.DirectoryEntryReferenceParam):
 		return parameters.NewDirectoryEntryParamFromString(parameterValue)
-	case "ENCODING":
+	case string(registries.EncodingParam):
 		return parameters.NewInlineEncodingParamFromString(parameterValue), nil
-	case "FBTYPE":
+	case string(registries.FreeBusyTimeTypeParam):
 		return parameters.NewFreeBusyTimeParamFromString(parameterValue), nil
-	case "FMTTYPE":
+	case string(registries.FormatTypeParam):
 		return parameters.NewFormatTypeParam(parameterValue), nil
-	case "LANGUAGE":
+	case string(registries.LanguageParam):
 		return parameters.NewLanguageParam(parameterValue), nil
-	case "MEMBER":
+	case string(registries.MemberParam):
 		return parameters.NewMemberParamFromString(parameterValue)
-	case "PARTSTAT":
+	case string(registries.ParticipationStatusParam):
 		return parameters.NewParticipationRoleParamFromString(parameterValue), nil
-	case "RELATED":
+	case string(registries.AlarmTriggerRelationshipParam):
 		return parameters.NewAlarmTriggerRelationshipParamFromString(parameterValue), nil
-	case "RANGE":
+	case string(registries.RecurrenceIdRangeParam):
 		return parameters.NewRecurrenceIdRangeParam(), nil
-	case "RELTYPE":
+	case string(registries.RelationshipTypeParam):
 		return parameters.NewRelationshipParamFromString(parameterValue), nil
-	case "ROLE":
+	case string(registries.ParticipantRoleParam):
 		return parameters.NewParticipationRoleParamFromString(parameterValue), nil
-	case "RSVP":
+	case string(registries.RsvpExpectationParam):
 		return parameters.NewRSVPParamFromString(parameterValue)
-	case "SENT-BY":
+	case string(registries.SentByParam):
 		return parameters.NewSentByParamFromString(parameterValue)
-	case "TZID":
+	case string(registries.TimeZoneIdParam):
 		return parameters.NewTimeZoneIdentifierParam(parameterValue)
-	case "VALUE":
+	case string(registries.ValueDataTypesParam):
 		return parameters.NewValueDataTypesParamFromString(parameterValue), nil
 	// Non standard properties
 	default:
