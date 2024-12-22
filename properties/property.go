07c5f267a7f4a60a4b340202d56b8d715708f962
Valentin REVERSAT <dev@vareversat.fr>
2024-12-22 20:45:11 +0000

    chore: run `make fmt`

--- properties/property.go
+++ properties/property.go
@@ -487,7 +487,9 @@ func (dP *durationPropertyType) ToICalendarPropFormat(output io.Writer) {
 		computeParameters(&paramsOutput, dP.Parameters)
 	}
 	unfoldedOutput.Write(
-		[]byte(fmt.Sprintf("%s%s:%s", dP.PropName, paramsOutput.String(), dP.Value.GetStringValue())),
+		[]byte(
+			fmt.Sprintf("%s%s:%s", dP.PropName, paramsOutput.String(), dP.Value.GetStringValue()),
+		),
 	)
 	foldOutput(&unfoldedOutput)
 	unfoldedOutput.WriteTo(output)
